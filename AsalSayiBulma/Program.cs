31038a224c3539a5a70607736b837205abf54290
arif0616 <arifkimlaa@gmail.com>
2020-04-20 05:19:40 +0200

    AsalSayiBulma

---
+++ AsalSayiBulma/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AsalSayiBulma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Ilk Sayiyi Giriniz:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ikinci Sayiyi Giriniz:");
+            int b = int.Parse(Console.ReadLine());
+            for (int i = 2; i <= a && i <= b;i++)
+            {
+                if (a % i == 0 && b % i == 0)
+                {
+                    Console.WriteLine("Tam bolunen Sayilar");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Sayilar Aralarinda Asaldir");
+                    break;
+                }
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
